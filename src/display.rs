fa8a656deac649ba7687f6de4deba1f7d11a65a6
Vlad Stepanov <8uk.8ak@gmail.com>
2023-02-28 19:24:02 +0400

    [init] simple app with two-photo changer

---
+++ src/display.rs
@@ -0,0 +1,98 @@
+use std::error::Error;
+
+use display_interface_spi::SPIInterfaceNoCS;
+use esp_idf_hal::{
+    delay::Ets,
+    gpio::{
+        AnyIOPin,
+        PinDriver,
+        Gpio18,
+        Gpio19,
+        Gpio23,
+        Gpio16,
+        Gpio5,
+        Gpio4,
+        InputOutput,
+    },
+    spi::{
+        config::Config,
+        Dma,
+        SpiDeviceDriver,
+        SpiDriver,
+        SPI2,
+    },
+    units::FromValueType,
+};
+use mipidsi::{Builder};
+
+type Display<'d> = mipidsi::Display<
+    SPIInterfaceNoCS<
+        SpiDeviceDriver<
+            'd,
+            SpiDriver<'d>,
+        >,
+        PinDriver<'d, Gpio16, InputOutput>,
+    >,
+    mipidsi::models::ST7789,
+    PinDriver<'d, Gpio23, InputOutput>,
+>;
+
+pub(crate) fn create_st7789_pico(
+    spi: SPI2,
+    sclk: Gpio18,
+    sdo: Gpio19,
+    rst: Gpio23,
+    dc: Gpio16,
+    cs: Gpio5,
+    bl: Gpio4,
+) -> Result<Display<'static>, Box<dyn Error>> {
+    // turn on backlight
+    let mut bl = PinDriver::input_output_od(bl)?;
+    bl.set_high()?;
+
+    let spi_driver = SpiDriver::new(
+        spi,
+        sclk,
+        sdo,
+        None::<AnyIOPin>,
+        Dma::Channel1(240 * 135 * 2 + 8),
+    )
+    .map_err(|e| format!("Failed to initialize SPI driver: {e:?}"))?;
+
+    let spi_config = Config::new()
+        .baudrate(80.MHz().into())
+        .write_only(true);
+
+    let spi = SpiDeviceDriver::new(spi_driver, Some(cs), &spi_config)
+        .map_err(|e| format!("Failed to initialize SPI device: {e:?}"))?;
+
+    let rst = PinDriver::input_output_od(rst)?;
+    let dc = PinDriver::input_output_od(dc)?;
+    let mut delay = Ets;
+
+    let di = SPIInterfaceNoCS::new(spi, dc);
+
+    let display = Builder::st7789_pico1(di)
+        .init(&mut delay, Some(rst))
+        .map_err(|e| format!("Failed to initialize display: {e:?}"))?;
+
+    Ok(display)
+}
+
+macro_rules! create {
+    ($peripherals: expr) => {{
+        let spi = $peripherals.spi2;
+        let sclk = $peripherals.pins.gpio18;
+        let sdo = $peripherals.pins.gpio19;
+
+        let rst = $peripherals.pins.gpio23;
+        let dc = $peripherals.pins.gpio16;
+
+        let cs = $peripherals.pins.gpio5;
+        let bl = $peripherals.pins.gpio4;
+
+        crate::display::create_st7789_pico(spi, sclk, sdo, rst, dc, cs, bl)?
+    }};
+}
+
+pub(crate) use create;
---
+++ src/main.rs
@@ -0,0 +1,99 @@
+use std::error::Error;
+
+use embedded_graphics::{
+    image::Image,
+    pixelcolor::Rgb565,
+    prelude::{DrawTarget, Point},
+    Drawable,
+};
+use esp_idf_hal::{
+    peripherals::Peripherals,
+    gpio::PinDriver,
+};
+use log::info;
+use tinybmp::Bmp;
+
+use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+
+mod display;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // It is necessary to call this function once. Otherwise some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_sys::link_patches();
+
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    let peripherals = Peripherals::take().expect("Failed to take peripherals");
+    let mut display = display::create!(peripherals);
+
+    let anya_bytes = include_bytes!("../anya.bmp");
+    let anya_bmp =
+        Bmp::from_slice(anya_bytes).map_err(|e| format!("Failed to parse BMP: {e:?}"))?;
+    let anya_img = Image::new(&anya_bmp, Point::new(0, 0));
+
+    let vlad_bytes = include_bytes!("../vlad.bmp");
+    let vlad_bmp =
+        Bmp::from_slice(vlad_bytes).map_err(|e| format!("Failed to parse BMP: {e:?}"))?;
+    let vlad_img = Image::new(&vlad_bmp, Point::new(0, 0));
+
+    display
+        .clear(Rgb565::new(0, 255, 255))
+        .map_err(|e| format!("Failed to clear display: {e:?}"))?;
+
+    info!("cleared display, press two buttons to clear it again");
+
+    let left_button = PinDriver::input(peripherals.pins.gpio0)?;
+    let right_button = PinDriver::input(peripherals.pins.gpio35)?;
+
+    let mut anya_shown = false;
+    let mut vlad_shown = false;
+
+    info!("initialized, waiting for button presses...");
+
+    loop {
+        match (left_button.is_low(), right_button.is_low()) {
+            (true, true) => {
+                if !(anya_shown || vlad_shown) {
+                    continue;
+                }
+
+                info!("Both buttons pressed, clearing display");
+                display
+                    .clear(Rgb565::new(0, 255, 255))
+                    .map_err(|e| format!("Failed to clear display: {e:?}"))?;
+
+                anya_shown = false;
+                vlad_shown = false;
+            }
+            (true, false) => {
+                if anya_shown {
+                    continue;
+                }
+
+                info!("Left button pressed, drawing Anya");
+                anya_img
+                    .draw(&mut display)
+                    .map_err(|e| format!("Failed to draw image: {e:?}"))?;
+
+                anya_shown = true;
+                vlad_shown = false;
+            }
+            (false, true) => {
+                if vlad_shown {
+                    continue;
+                }
+
+                info!("Right button pressed, drawing Vlad");
+                vlad_img
+                    .draw(&mut display)
+                    .map_err(|e| format!("Failed to draw image: {e:?}"))?;
+
+                anya_shown = false;
+                vlad_shown = true;
+            }
+            _ => {}
+        }
+    }
+}
